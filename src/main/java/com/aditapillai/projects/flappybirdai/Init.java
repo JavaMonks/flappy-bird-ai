266f9df1127b2e987b036fc56e4d594e8650fae6
unsubd <aditapillai@gmail.com>
2020-06-23 01:34:00 +0530

    (create) : initial setup

---
+++ src/main/java/com/aditapillai/projects/flappybirdai/Init.java
@@ -0,0 +1,10 @@
+package com.aditapillai.projects.flappybirdai;
+
+import com.aditapillai.projects.flappybirdai.game.Game;
+import processing.core.PApplet;
+
+public class Init {
+    public static void main(String[] args) {
+        PApplet.main(Game.class, args);
+    }
+}
---
+++ src/main/java/com/aditapillai/projects/flappybirdai/game/Game.java
@@ -0,0 +1,16 @@
+package com.aditapillai.projects.flappybirdai.game;
+
+import processing.core.PApplet;
+
+public class Game extends PApplet {
+
+    @Override
+    public void settings() {
+        this.size(800, 640);
+    }
+
+    @Override
+    public void setup() {
+
+    }
+}
