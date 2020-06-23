edd13fb7e476ac52a9d99e1430fb35304d5e9926
unsubd <aditapillai@gmail.com>
2020-06-23 11:40:00 +0530

    (refactor) : improved variable names

--- src/main/java/com/aditapillai/projects/flappybirdai/game/utils/CollisionUtils.java
+++ src/main/java/com/aditapillai/projects/flappybirdai/game/utils/CollisionUtils.java
@@ -5,20 +5,20 @@ import com.aditapillai.projects.flappybirdai.game.entities.Pipe;
 
 public class CollisionUtils {
 
-    public static boolean birdHitBorder(Bird bird, float height) {
-        boolean hitBottom = (bird.getY() + bird.getRadius()) >= height;
+    public static boolean birdHitBorder(Bird bird, float windowHeight) {
+        boolean hitBottom = (bird.getY() + bird.getRadius()) >= windowHeight;
         boolean hitTop = (bird.getY()) <= bird.getRadius();
         return hitBottom || hitTop;
     }
 
     public static boolean birdHitPipe(Bird bird, Pipe pipe) {
         float birdX = bird.getX() + bird.getRadius();
-        float birdYTop = bird.getY() - bird.getRadius();
-        float birdYBottom = bird.getY() + bird.getRadius();
+        float birdTop = bird.getY() - bird.getRadius();
+        float birdBottom = bird.getY() + bird.getRadius();
 
         boolean isWithinPipeX = birdX >= pipe.getX() && birdX <= pipe.getX() + pipe.getWidth();
-        boolean hitTopPipe = isWithinPipeX && (birdYTop <= pipe.getTopLength());
-        boolean hitBottomPipe = isWithinPipeX && (birdYBottom >= pipe.getTopLength() + pipe.getSpace());
+        boolean hitTopPipe = isWithinPipeX && (birdTop <= pipe.getTopLength());
+        boolean hitBottomPipe = isWithinPipeX && (birdBottom >= pipe.getTopLength() + pipe.getSpace());
 
         return hitTopPipe || hitBottomPipe;
     }
