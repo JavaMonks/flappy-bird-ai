f267f11e361c753a4455976af0ec96773f24f739
unsubd <aditapillai@gmail.com>
2020-06-23 10:47:42 +0530

    (develop) : bird dies if it hit the borders

--- src/main/java/com/aditapillai/projects/flappybirdai/game/entities/Bird.java
+++ src/main/java/com/aditapillai/projects/flappybirdai/game/entities/Bird.java
@@ -10,22 +10,45 @@ public class Bird {
     private final PVector gravity;
     private final PVector jump;
 
+    private final int birdDiam;
+    private final int birdRadius;
+    private boolean alive = true;
+
     public Bird(Game game) {
         this.game = game;
         this.position = new PVector(50, (float) game.height / 2);
         this.gravity = new PVector(0, 0.8f);
         this.jump = new PVector(0, -10);
+        this.birdDiam = 50;
+        this.birdRadius = this.birdDiam / 2;
     }
 
     public void show() {
-        this.game.ellipse(this.position.x, this.position.y, 50, 50);
+        this.game.ellipse(this.position.x, this.position.y, this.birdDiam, this.birdDiam);
     }
 
     public void update() {
-        this.position.add(this.gravity);
+        if (alive) {
+            this.position.add(this.gravity);
+            if (this.hitBorder()) {
+                this.kill();
+            }
+        }
     }
 
     public void jump() {
-        this.position.add(this.jump);
+        if (alive) {
+            this.position.add(this.jump);
+        }
+    }
+
+    public void kill() {
+        this.alive = false;
+    }
+
+    private boolean hitBorder() {
+        boolean hitBottom = (this.position.y + this.birdRadius) >= this.game.height;
+        boolean hitTop = (this.position.y) <= this.birdRadius;
+        return hitBottom || hitTop;
     }
 }
